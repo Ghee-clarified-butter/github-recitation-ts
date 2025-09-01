025b724542a14a24bbcac1d77191f79347cbe563
Anurag Aryal <aryalanurag20602003@gmail.com>
2025-09-01 19:06:09 +0300

    fibRoute.ts fixed

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,18 @@
-// Endpoint for querying the fibonacci numbers
+import { Request, Response } from 'express';
+import fibonacci from './fib';
 
-const fibonacci = require("./fib");
-
-export default (req, res) => {
+export default (req: Request, res: Response): void => {
   const { num } = req.params;
-
-  const fibN = fibonacci(parseInt(num));
+  
+  // Ensure num is a string and can be parsed as integer
+  const numValue = parseInt(num as string, 10);
+  
+  const fibN = fibonacci(numValue);
   let result = `fibonacci(${num}) is ${fibN}`;
-
+  
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
   }
-
+  
   res.send(result);
 };
